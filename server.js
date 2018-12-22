ff2557a2f541920970e67080c7b895659c3a83fe
Ethan Smoller <ethansmoller@gmail.com>
2018-12-22 12:28:13 -0800

    Added basic routing

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+// Requires
 const express = require("express");
+const bodyParser = require("body-parser");
 
+// Initialize Express app
 const app = express();
+
+// Body Parser magic
+app.use(bodyParser.urlencoded( { extended: false }))
+
+// Enable static files
+// app.use(express.static('public'));
 
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Is this thing on?");
+  res.sendFile(__dirname + "/app/public/home.html");
+});
+
+app.get("/survey", (req, res) => {
+  res.sendFile(__dirname + "/app/public/survey.html");
 });
 
 app.listen(PORT, () => {

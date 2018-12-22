ca810c4bcf9311134490b0191d6a98c64d71b5ad
Ethan Smoller <ethansmoller@gmail.com>
2018-12-22 00:34:03 -0800

    Changed some port stuff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Is this thing on?");
